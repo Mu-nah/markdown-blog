e6a374f14a3f58459732ff94aea3e5ee41975682
Mu-nah <102527479+Mu-nah@users.noreply.github.com>
2022-12-27 02:07:57 +0100

    Add files via upload

--- server.js
+++ server.js
@@ -1,43 +1,46 @@
-const express = require('express')
-const bcrypt = require('bcryptjs')
-const cookieParser = require('cookie-parser')
-const jwt = require('jsonwebtoken')
-const mongoose = require('mongoose')
-const Article = require('./models/article')
-const User = require('./models/user')
-const path = require('path')
-const articleRouter = require('./routes/articles')
-const userRouter = require('./routes/user')
-const methodOverride = require('method-override')
-const app = express()
-const  {signup, signin} = require('./controllers/auth.controllers.js') 
-const  {verifyToken} = require('./middlewares/auth.js') 
+const express = require('express');
+const bcrypt = require('bcryptjs');
+const cookieParser = require('cookie-parser');
+const jwt = require('jsonwebtoken');
+const mongoose = require('mongoose');
+const Article = require('./models/article');
+const User = require('./models/user');
+const path = require('path');
+const articleRouter = require('./routes/articles');
+const userRouter = require('./routes/user');
+const methodOverride = require('method-override');
+const app = express();
+const { signup, signin } = require('./controllers/auth.controllers.js');
+const { verifyToken } = require('./middlewares/auth.js');
 
-const dbURI=''
+require('dotenv').config();
 
-mongoose.connect(dbURI, {
-  useNewUrlParser: true, useUnifiedTopology: true 
-})
-.then( app.listen(3000, ()=>{
-  console.log('connected to db and running')}))
-.catch((err) => console.log(err));
+const PORT = process.env.PORT;
 
-app.set('view engine', 'ejs')
-app.use(express.static('uploads'))
-app.use(express.urlencoded({ extended: false }))
-app.use('/uploads', express.static('uploads')) 
-app.use(express.static('views'))
-app.use(methodOverride('_method'))
-app.use(express.static(__dirname + '/views'))
- app.use(express.static(path.join(__dirname, "views")))
- app.use(cookieParser())
- 
- 
+mongoose
+  .connect(process.env.dbURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(
+    app.listen(PORT, () => {
+      console.log(`connected to db and running on ${PORT}`);
+    })
+  )
+  .catch((err) => console.log(err));
+
+app.set('view engine', 'ejs');
+app.use(express.urlencoded({ extended: false }));
+app.use('/uploads', express.static('uploads'));
+app.use(express.static('views'));
+app.use(methodOverride('_method'));
+app.use(express.static(__dirname + '/views'));
+app.use(express.static(path.join(__dirname, 'views')));
+app.use(cookieParser());
+
 app.get('/', (req, res) => {
-  res.render('articles/home')
-})
+  res.render('articles/home');
+});
 
-
-app.use('/articles', articleRouter)
-app.use('/user', userRouter)
-
+app.use('/articles', articleRouter);
+app.use('/user', userRouter);
