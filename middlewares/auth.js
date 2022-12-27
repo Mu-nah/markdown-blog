005277657e80c4a41ef5aa8e917a276c7cfc94cb
Mu-nah <102527479+Mu-nah@users.noreply.github.com>
2022-12-27 02:52:44 +0100

    modified

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,24 +1,27 @@
-const jwt = require("jsonwebtoken");
-const cookieParser = require('cookie-parser')
-const User = require('../models/user')
+const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser');
+const User = require('../models/user');
 
-const API_SECRET = ""
+const API_SECRET = process.env.API_SECRET;
 
 exports.verifyToken = async (req, res, next) => {
   const token =
-    req.body.token || req.query.token || req.cookies['x-access-token'] || req.headers["x-access-token"];
+    req.body.token ||
+    req.query.token ||
+    req.cookies['x-access-token'] ||
+    req.headers['x-access-token'];
 
   if (!token) {
-     //return res.status(403).send("A token is required for authentication");
-  res.redirect('/user/login')
-  }else{
-  try {
-    const decoded = await jwt.verify(token, API_SECRET); 
-   req.user = decoded
-  console.log(req.user)
-  
-  } catch (err) {
-    res.redirect('/user/login')
+    //return res.status(403).send("A token is required for authentication");
+    res.redirect('/user/login');
+  } else {
+    try {
+      const decoded = await jwt.verify(token, API_SECRET);
+      req.user = decoded;
+      console.log(req.user);
+    } catch (err) {
+      res.redirect('/user/login');
+    }
+    return next();
   }
-  return next();} 
 };
